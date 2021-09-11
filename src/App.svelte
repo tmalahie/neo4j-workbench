db838d26506653908dd0316634a466bd95eb23c8
tmalahie <t.malahieude@gmail.com>
2021-09-11 10:37:57 +0200

    Update menu using store

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,10 @@
   import Router from "svelte-spa-router";
   import Home from "./routes/Home.svelte";
   import NotFound from "./routes/NotFound.svelte";
-  import { Accordion, Icon } from "sveltestrap";
   import Connection from "./routes/Connection.svelte";
   import Connections from "./routes/Connections.svelte";
   import { Bootbox } from "bootbox-svelte";
-  import { link } from "svelte-spa-router";
+  import Menu from "./components/Menu.svelte";
 
   const routes = {
     "/": Home,
@@ -18,16 +17,7 @@
 </script>
 
 <main class="App">
-  <div class="drawer">
-    <Accordion>
-      <a class="accordion-item" href="/" use:link
-        ><Icon name="house-door" /> Home</a
-      >
-      <a class="accordion-item" href="/connections" use:link
-        ><Icon name="box" /> Connections</a
-      >
-    </Accordion>
-  </div>
+  <Menu />
   <div class="body">
     <Router {routes} />
     <Bootbox />
@@ -35,9 +25,6 @@
 </main>
 
 <style lang="scss">
-  @import "node_modules/bootstrap/scss/functions";
-  @import "node_modules/bootstrap/scss/variables";
-
   :global(body) {
     margin: 0;
     padding: 0;
@@ -45,24 +32,6 @@
   .App {
     display: flex;
     height: 100vh;
-
-    .drawer {
-      width: 10em;
-      height: 100%;
-      overflow-y: auto;
-      :global(.accordion-item) {
-        padding: 0.5em 1em;
-      }
-      :global(.accordion) {
-        height: 100%;
-        border-right: 1px solid rgba(0, 0, 0, 0.125);
-      }
-      a {
-        display: block;
-        color: $dark;
-        text-decoration: none;
-      }
-    }
     .body {
       flex: 1;
       padding: 1em;
---
+++ src/components/Menu.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { Accordion, AccordionItem, Icon } from "sveltestrap";
+  import { link } from "svelte-spa-router";
+  import connections from "src/stores/connections";
+</script>
+
+<div class="drawer">
+  <Accordion>
+    <a class="accordion-item accordion-single" href="/" use:link
+      ><Icon name="house-door" /> Home</a
+    >
+    <AccordionItem header="Connections">
+      {#each $connections as connection}
+        <a href={`/connections/${connection.id}`} use:link
+          ><Icon name="box" /> {connection.name}</a
+        >
+      {/each}
+    </AccordionItem>
+  </Accordion>
+</div>
+
+<style lang="scss">
+  @import "node_modules/bootstrap/scss/functions";
+  @import "node_modules/bootstrap/scss/variables";
+
+  .drawer {
+    width: 10em;
+    height: 100%;
+    overflow-y: auto;
+    :global(.accordion-item.accordion-single) {
+      padding: 0.5em 1em;
+    }
+    :global(.accordion) {
+      height: 100%;
+      border-right: 1px solid rgba(0, 0, 0, 0.125);
+    }
+    a {
+      display: block;
+      color: $dark;
+      text-decoration: none;
+    }
+  }
+</style>
