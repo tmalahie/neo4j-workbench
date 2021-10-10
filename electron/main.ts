20b32ebb39caeea9b822546a2f99864ac6df1197
tmalahie <t.malahieude@gmail.com>
2021-10-10 22:10:07 +0200

    Add favicon

--- electron/main.ts
+++ electron/main.ts
@@ -25,6 +25,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + "/../../public/favicon.png",
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
